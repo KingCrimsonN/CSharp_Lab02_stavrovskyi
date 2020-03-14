487a40294b3410eacce6e0f7ffbd2ba0d654873c
KingCrimsonN <48062712+KingCrimsonN@users.noreply.github.com>
2020-03-14 18:27:23 +0200

    Lab03

---
+++ Sharp_lab03_stavrovskyi/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+
+using Sharp_lab03_stavrovskyi.ViewModels;
+
+namespace Sharp_lab03_stavrovskyi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ Sharp_lab03_stavrovskyi/Views/LoginWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using Sharp_lab03_stavrovskyi.ViewModels;
+
+namespace Sharp_lab03_stavrovskyi
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : UserControl
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+    }
+}
